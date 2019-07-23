9250d5bc8cbf1b7003589245d6ad95ddde231bc3
huynhphucap <huynhphucap@gmail.com>
2019-07-23 17:21:43 +0700

    update layout home

--- Angular/src/app/home/home.component.ts
+++ Angular/src/app/home/home.component.ts
@@ -9,16 +9,17 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
   data: Array<any> = [
-    {label: 'Aladin', text: 'aaaaaaaaaaaaaaassssssssssssssssssssssssssssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaay'},
+    {label: 'Aladin', text: 'Norm Of The North 2: Keys To The Kingdom'},
     {label: 'Doremon', text: 'Norm Of The North 2: Keys To The Kingdom'},
-    {label: 'Conan', text: 'The Lego Movie 2'},
-    {label: 'Aladin', text: 'nội dung ở đây'},
-    {label: 'Aladin', text: 'nội dung ở đây'},
-    {label: 'Aladin', text: 'nội dung ở đây'},
+    {label: 'Conan', text: 'The Lego Movie 22222222222222222222222 The Lego Movie 22222222222222222222222'},
     {label: 'Aladin', text: 'nội dung ở đây'},
     {label: 'Aladin', text: 'nội dung ở đây'},
 
   ]
+  isCollapsed = true;
+  hide(){
+    this.isCollapsed = !this.isCollapsed;
+  }
   ngOnInit() {
   }
 
